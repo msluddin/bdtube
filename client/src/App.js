eb929d6a0dc88a304613f5a7fb50e1ac2fab6d61
msluddin <msluddin@gmail.com>
2022-09-28 01:08:55 +0600

    Search

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Video from './pages/Video';
 import SignIn from './pages/SignIn';
+import Search from './pages/Search';
 
 const Container = styled.div`
   display: flex;
@@ -37,6 +38,7 @@ function App() {
                   <Route path="trends" element={<Home type="trend" />} />
                   <Route path="subscriptions" element={<Home type="sub" />} />
                   <Route path="signin" element={<SignIn />} />
+                  <Route path="search" element={<Search />} />
                   <Route path="video">
                     <Route path=":id" element={<Video />} />
                   </Route>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../redux/userSlice';
 import Upload from './Upload';
@@ -36,6 +36,7 @@ const Search = styled.div`
   padding: 5px;
   border: 1px solid #ccc;
   border-radius: 3px;
+  color: ${({ theme }) => theme.text};
 `;
 
 const Input = styled.input`
@@ -74,8 +75,10 @@ const Avatar = styled.img`
 `;
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
   const [open, setOpen] = useState(false);
+  const [q, setQ] = useState('');
   const dispatch = useDispatch();
 
   const logOut = () => {
@@ -87,8 +90,11 @@ const Navbar = () => {
       <Container>
         <Wrapper>
           <Search>
-            <Input placeholder="Search" />
-            <SearchOutlinedIcon />
+            <Input
+              placeholder="Search"
+              onChange={(e) => setQ(e.target.value)}
+            />
+            <SearchOutlinedIcon onClick={() => navigate(`/search?q=${q}`)} />
           </Search>
           {currentUser ? (
             <User>
